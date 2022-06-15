375b0c32e88f8dc3cb781f66b1c7d4854d513b44
ankushom1910 <atharvaachutam19@gmail.com>
2022-06-15 15:38:29 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Soft Portfolio
+
+A neumorphic inspired personal website and portfolio.
+- Showcase your project screenshots with carousels
+- Link up your social and other links
+- Supports light and dark mode 💡
+
+### Configurations
+- Under `src/data/fixtures.ts`, edit to customise copy.
+
+- Under `src/themes`, edit light and dark themes (including background image).
+
+- Render your own 3D model by editing `blender/peregrine-falcon.blend` using [Blender](https://www.blender.org/) for free.
+
+- Download company logos and use [Figma](https://www.figma.com/) to edit and export svg with no borders.
+
+Push your changes to master to auto-deploy to vercel.
+
+## Development
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn start
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
---
+++ src/data/fixtures.ts
@@ -0,0 +1,144 @@
+export default {
+  author: "{𝒸❁𝓂𝓅𝓊𝓉𝑒𝓇%} (!𝒹𝑒𝓅𝒶𝓇𝓉𝓂𝑒𝓃𝓉.)",
+  hero: {
+    subtitle: "Department of RKMV Biennial Exhibition",
+    description: "Description of Our Department ipsum dolor sit amet, consectetur adipiscing elitpsum dolor sit amet, consectetur adipiscing elit.",
+    cta: "Browse Projects",
+  },
+  products: {
+    featured: [
+      {
+        name: "Test",
+        medium: "Technologies Used",
+        summary: "Crisp Lines",
+        copy: [
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a ipsum sit amet nulla venenatis suscipit. Vivamus quis euismod lacus. Nunc ac justo diam."
+        ],
+        links: {
+          color: "#363636",
+          primary: {
+            name: "Links To The Code",
+            url: "https://github.me/"
+          }
+        },
+        images: [
+          "https://media3.giphy.com/media/11YMFLRM0gWZ1u/200w.webp?cid=ecf05e47ecj93sahgqj2dlix22xf6d7lpnpdeplgdwcc3z8r&rid=200w.webp&ct=ghttps://i0.wp.com/sixcolors.com/wp-content/uploads/2021/06/monterey-intro2-6c.jpg?ssl=1",
+
+        ]
+      },
+      {
+        name: "Test",
+        medium: "Technologies Used",
+        summary: "Crisp Lines",
+        copy: [
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a ipsum sit amet nulla venenatis suscipit. Vivamus quis euismod lacus. Nunc ac justo diam."
+        ],
+        links: {
+          color: "#363636",
+          primary: {
+            name: "Links To The Code",
+            url: "https://github.me/"
+          }
+        },
+        images: [
+          "https://media2.giphy.com/media/BaLggE7K8JBXa/200w.webp?cid=ecf05e47ecj93sahgqj2dlix22xf6d7lpnpdeplgdwcc3z8r&rid=200w.webp&ct=g",
+        
+        ]
+      },
+      {
+        name: "Test",
+        medium: "Technologies Used",
+        summary: "Crisp Lines",
+        copy: [
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a ipsum sit amet nulla venenatis suscipit. Vivamus quis euismod lacus. Nunc ac justo diam."
+        ],
+        links: {
+          color: "#363636",
+          primary: {
+            name: "Links To The Code",
+            url: "https://github.me/"
+          }
+        },
+        images: [
+          "https://media1.giphy.com/media/Vt7tALoHXmcfe/200w.webp?cid=ecf05e47pnls6e9j843j9yn0y0k91347x7nvfyx9t35nvltm&rid=200w.webp&ct=g",
+          "https://media4.giphy.com/media/Gev6zrYpjpYWY/200w.webp?cid=ecf05e47pnls6e9j843j9yn0y0k91347x7nvfyx9t35nvltm&rid=200w.webp&ct=g",
+          "https://media0.giphy.com/media/YthBSgzZfuTJu/giphy.gif?cid=ecf05e4725cz4ysz22zxqk0i2o1n8jbx0bnerwuj2fmh79h4&rid=giphy.gif&ct=g"
+        ]
+      }
+    ],
+    // Only supports 0-3 at the moment
+    minor: [
+      {
+        name: "Small Minor Projects",
+        description: "Description",
+        link: {
+          name: "Github",
+          url: "https://github.com/"
+        }
+      },
+      {
+        name: "Small Minor Projects",
+        description: "Description",
+        link: {
+          name: "Github",
+          url: "https://github.com/"
+        }
+      },
+      {
+        name: "Small Minor Projects",
+        description: "Description",
+        link: {
+          name: "Github",
+          url: "https://github.com/"
+        }
+      }
+    ]
+  },
+  logoSection: {
+    title: "Some Text",
+    // Only supports 0-3 at the moment
+    logos: [
+      {
+        src: "Link To Logo",
+        url: "link",
+        alt: "Logo"
+      },
+      {
+        src: "Link To Logo",
+        url: " link",
+        alt: "Logo"
+      },
+      {
+        src: " Link To Logo",
+        url: "link",
+        alt: "Logo"
+      }
+  ]
+  },
+  footer: {
+    tagline: "Made By Class 10 with 🧠 ",
+    links: [
+      {
+        name: "Twitter",
+        url: "https://twitter.com/ankushom6",
+        fa: "fab fa-twitter" // Code from: https://fontawesome.com/v5/icons/twitter?s=brands
+      },
+
+      {
+        name: "Github",
+        url: "https://github.com/ankushom1910",
+        fa: "fab fa-github"
+      },
+      {
+        name: "Official School Website",
+        url: "https://rkmvdeoghar.org",
+        fa: "fa fa-globe"
+      },
+      {
+        name: "Report Bug",
+        url: "mailto:ankush19om@gmail.com",
+        fa: "fa fa-bug"
+      },
+    ]
+  }
+}
