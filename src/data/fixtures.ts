8aa58f2319522a1f5f06c1d89aba9ba0a83e88d0
ankushom19 <atharvaachutam19@gmail.com>
2022-06-18 23:21:47 +0530

    some changes

--- src/data/fixtures.ts
+++ src/data/fixtures.ts
@@ -2,11 +2,30 @@ export default {
   author: "{𝒸❁𝓂𝓅𝓊𝓉𝑒𝓇%} (!𝒹𝑒𝓅𝒶𝓇𝓉𝓂𝑒𝓃𝓉.)",
   hero: {
     subtitle: "Department of RKMV Biennial Exhibition",
-    description: "Description of Our Department ipsum dolor sit amet, consectetur adipiscing elitpsum dolor sit amet, consectetur adipiscing elit.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis posuere dolor quis pulvinar. Aliquam non felis vel eros mattis faucibus ut in arcu.  ",
     cta: "Browse Projects",
   },
   products: {
     featured: [
+      {
+        name: "Test",
+        medium: "Technologies Used",
+        summary: "Crisp Lines",
+        copy: [
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a ipsum sit amet nulla venenatis suscipit. Vivamus quis euismod lacus. Nunc ac justo diam."
+        ],
+        links: {
+          color: "#363636",
+          primary: {
+            name: "Links To The Code",
+            url: "https://github.me/"
+          }
+        },
+        images: [
+          "https://media3.giphy.com/media/11YMFLRM0gWZ1u/200w.webp?cid=ecf05e47ecj93sahgqj2dlix22xf6d7lpnpdeplgdwcc3z8r&rid=200w.webp&ct=ghttps://i0.wp.com/sixcolors.com/wp-content/uploads/2021/06/monterey-intro2-6c.jpg?ssl=1",
+
+        ]
+      },
       {
         name: "Test",
         medium: "Technologies Used",
@@ -60,6 +79,45 @@ export default {
           }
         },
         images: [
+          "https://media2.giphy.com/media/BaLggE7K8JBXa/200w.webp?cid=ecf05e47ecj93sahgqj2dlix22xf6d7lpnpdeplgdwcc3z8r&rid=200w.webp&ct=g",
+        
+        ]
+      },
+      {
+        name: "Test",
+        medium: "Technologies Used",
+        summary: "Crisp Lines",
+        copy: [
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a ipsum sit amet nulla venenatis suscipit. Vivamus quis euismod lacus. Nunc ac justo diam."
+        ],
+        links: {
+          color: "#363636",
+          primary: {
+            name: "Links To The Code",
+            url: "https://github.me/"
+          }
+        },
+        images: [
+          "https://media1.giphy.com/media/Vt7tALoHXmcfe/200w.webp?cid=ecf05e47pnls6e9j843j9yn0y0k91347x7nvfyx9t35nvltm&rid=200w.webp&ct=g",
+          "https://media4.giphy.com/media/Gev6zrYpjpYWY/200w.webp?cid=ecf05e47pnls6e9j843j9yn0y0k91347x7nvfyx9t35nvltm&rid=200w.webp&ct=g",
+          "https://media0.giphy.com/media/YthBSgzZfuTJu/giphy.gif?cid=ecf05e4725cz4ysz22zxqk0i2o1n8jbx0bnerwuj2fmh79h4&rid=giphy.gif&ct=g"
+        ]
+      },
+      {
+        name: "Test",
+        medium: "Technologies Used",
+        summary: "Crisp Lines",
+        copy: [
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a ipsum sit amet nulla venenatis suscipit. Vivamus quis euismod lacus. Nunc ac justo diam."
+        ],
+        links: {
+          color: "#363636",
+          primary: {
+            name: "Links To The Code",
+            url: "https://github.me/"
+          }
+        },
+        images: [
           "https://media1.giphy.com/media/Vt7tALoHXmcfe/200w.webp?cid=ecf05e47pnls6e9j843j9yn0y0k91347x7nvfyx9t35nvltm&rid=200w.webp&ct=g",
           "https://media4.giphy.com/media/Gev6zrYpjpYWY/200w.webp?cid=ecf05e47pnls6e9j843j9yn0y0k91347x7nvfyx9t35nvltm&rid=200w.webp&ct=g",
           "https://media0.giphy.com/media/YthBSgzZfuTJu/giphy.gif?cid=ecf05e4725cz4ysz22zxqk0i2o1n8jbx0bnerwuj2fmh79h4&rid=giphy.gif&ct=g"
@@ -92,37 +150,17 @@ export default {
           url: "https://github.com/"
         }
       }
-    ]
+    ],
+    
   },
   logoSection: {
-    title: "Some Text",
+    title: "Thank You !",
     // Only supports 0-3 at the moment
-    logos: [
-      {
-        src: "Link To Logo",
-        url: "link",
-        alt: "Logo"
-      },
-      {
-        src: "Link To Logo",
-        url: " link",
-        alt: "Logo"
-      },
-      {
-        src: " Link To Logo",
-        url: "link",
-        alt: "Logo"
-      }
-  ]
+
   },
   footer: {
-    tagline: "Made By Om",
+    tagline: "Made with ❤ by Me😉",
     links: [
-      {
-        name: "Twitter",
-        url: "https://twitter.com/ankushom6",
-        fa: "fab fa-twitter" // Code from: https://fontawesome.com/v5/icons/twitter?s=brands
-      },
 
       {
         name: "Github",
@@ -131,7 +169,7 @@ export default {
       },
       {
         name: "Official School Website",
-        url: "https://rkmvdeoghar.org",
+        url: "https://exhbitionrkmv.space",
         fa: "fa fa-globe"
       },
       {
